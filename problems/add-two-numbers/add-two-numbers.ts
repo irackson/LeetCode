08feb464b844ed6519c6b7b96d8ce8490df7008c
irackson <inasusr@gmail.com>
2024-09-03 20:25:26 -0400

    init add-two-numbers

---
+++ problems/add-two-numbers/add-two-numbers.ts
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=2 lang=typescript
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+
+function addTwoNumbers(
+    l1: ListNode | null,
+    l2: ListNode | null
+): ListNode | null {
+    return null;
+}
+// @lc code=end
