14d05b709322a3421c8b9d299a2f3f53ae2b00da
irackson <inasusr@gmail.com>
2024-10-04 16:40:41 -0400

    init two-sum

---
+++ problems/two-sum/two-sum.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] [Two Sum](https://leetcode.com/problems/two-sum/)
+# https://neetcode.io/problems/two-integer-sum
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        l: List[int] = []
+        return l
+
+
+# @lc code=end
