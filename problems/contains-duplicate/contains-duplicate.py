b0b0eb7a9c241bf513f1ff0c9da6fcd8ed82e8b6
irackson <inasusr@gmail.com>
2024-10-04 00:57:01 -0400

    init contains contains-duplicate

---
+++ problems/contains-duplicate/contains-duplicate.py
@@ -0,0 +1,12 @@
+# https://neetcode.io/problems/duplicate-integer
+#
+# @lc app=leetcode id=217 lang=python3
+#
+# [217] Contains Duplicate
+#
+
+# @lc code=start
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+# @lc code=end
