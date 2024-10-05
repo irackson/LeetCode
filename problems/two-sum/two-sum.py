0183eeb7634385cb78b709f04ce2fa3ce1b3e2cd
irackson <inasusr@gmail.com>
2024-10-05 12:25:45 -0400

    complete two-sum (inefficient implementation)

--- problems/two-sum/two-sum.py
+++ problems/two-sum/two-sum.py
@@ -9,10 +9,22 @@ from typing import List
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def properlyTypedTwoSum(self, nums: List[int], target: int) -> tuple[int, int]:
+        for index_a, value_a in enumerate(nums):
+            for index_b, value_b in enumerate(nums):
+                if index_a == index_b:
+                    continue
+                if value_a + value_b == target:
+                    return (index_a, index_b)
 
-        l: List[int] = []
-        return l
+        raise ValueError("no sum equal to target in provided nums")
+
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        return list(self.properlyTypedTwoSum(nums, target))
 
 
 # @lc code=end
+if __name__ == "__main__":
+    solution = Solution()
+    result = solution.twoSum([3, 2, 4], 6)
+    print(f"{result = }")
